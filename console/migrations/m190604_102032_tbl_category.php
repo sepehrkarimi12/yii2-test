2aaf4ad50004e54b0215182ffd16cef145a8edcf
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-06-04 15:26:10 +0430

    category table crated

---
+++ console/migrations/m190604_102032_tbl_category.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190604_102032_tbl_category
+ */
+class m190604_102032_tbl_category extends Migration
+{
+
+    private $table = 'tbl_category';
+
+    public function up()
+    {
+        $this->createTable($this->table,[
+            'id' => $this->primaryKey(),
+            'parent_id' => $this->integer(),
+            'title' => $this->string(),
+            'sort' => $this->integer(),
+            'status' => $this->boolean()
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropeTable($this->table);
+    }
+    
+}
