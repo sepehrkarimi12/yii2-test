618c6e34692ffdd3c24d4dd4fe705f6a311f0288
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-06-22 13:22:03 +0430

    lostForDropDown trait added to backend/modules/ad_type/models/AdType.php

--- backend/modules/ad_type/models/AdType.php
+++ backend/modules/ad_type/models/AdType.php
@@ -2,7 +2,7 @@
 
 namespace backend\modules\ad_type\models;
 
-use Yii;
+use common\traits\listForDropDown;
 
 /**
  * This is the model class for table "tbl_ad_type".
@@ -14,6 +14,7 @@ use Yii;
  */
 class AdType extends \yii\db\ActiveRecord
 {
+    use listForDropDown;
     /**
      * {@inheritdoc}
      */
