e91320c76289a527a79a168e9ede4424d74864b0
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-06-24 10:34:05 +0430

    common/models/TblAd.php created with gii for removal errors i get when creating models of other tables

---
+++ common/models/TblAd.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tbl_ad".
+ *
+ * @property int $id
+ * @property int $category_id
+ * @property int $city_id
+ * @property int $city_range_id
+ * @property string $title
+ * @property string $desc
+ * @property string $price
+ * @property string $mobile
+ * @property string $org_pic
+ * @property int $pic_counts
+ * @property int $immediate
+ * @property int $chat
+ * @property int $exchange
+ * @property int $expired
+ * @property int $user_id
+ * @property int $created_at
+ * @property int $updated_at
+ * @property int $published_at
+ *
+ * @property TblCategory $category
+ * @property TblCityRange $cityRange
+ * @property User $user
+ * @property TblApartmentRent[] $tblApartmentRents
+ * @property TblGeography[] $tblGeographies
+ * @property TblImage[] $tblImages
+ */
+class TblAd extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'tbl_ad';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['category_id', 'title', 'desc', 'mobile'], 'required'],
+            [['category_id', 'city_id', 'city_range_id', 'pic_counts', 'immediate', 'chat', 'exchange', 'expired', 'user_id', 'created_at', 'updated_at', 'published_at'], 'integer'],
+            [['title'], 'string', 'max' => 50],
+            [['desc'], 'string', 'max' => 255],
+            [['price'], 'string', 'max' => 20],
+            [['mobile'], 'string', 'max' => 11],
+            [['org_pic'], 'string', 'max' => 40],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => TblCategory::className(), 'targetAttribute' => ['category_id' => 'id']],
+            [['city_range_id'], 'exist', 'skipOnError' => true, 'targetClass' => TblCityRange::className(), 'targetAttribute' => ['city_range_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'category_id' => 'Category ID',
+            'city_id' => 'City ID',
+            'city_range_id' => 'City Range ID',
+            'title' => 'Title',
+            'desc' => 'Desc',
+            'price' => 'Price',
+            'mobile' => 'Mobile',
+            'org_pic' => 'Org Pic',
+            'pic_counts' => 'Pic Counts',
+            'immediate' => 'Immediate',
+            'chat' => 'Chat',
+            'exchange' => 'Exchange',
+            'expired' => 'Expired',
+            'user_id' => 'User ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'published_at' => 'Published At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(TblCategory::className(), ['id' => 'category_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCityRange()
+    {
+        return $this->hasOne(TblCityRange::className(), ['id' => 'city_range_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTblApartmentRents()
+    {
+        return $this->hasMany(TblApartmentRent::className(), ['ad_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTblGeographies()
+    {
+        return $this->hasMany(TblGeography::className(), ['ad_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTblImages()
+    {
+        return $this->hasMany(TblImage::className(), ['ad_id' => 'id']);
+    }
+}
