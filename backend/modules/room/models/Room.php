c14d7f2537c8af324e75373a384629c97527cd3c
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-06-24 10:04:36 +0430

    backend/modules/room/models/Room.php created

---
+++ backend/modules/room/models/Room.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace backend\modules\room\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tbl_room".
+ *
+ * @property int $id
+ * @property string $title
+ *
+ * @property TblApartmentRent[] $tblApartmentRents
+ */
+class Room extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'tbl_room';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'string', 'max' => 20],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTblApartmentRents()
+    {
+        return $this->hasMany(TblApartmentRent::className(), ['room_count_id' => 'id']);
+    }
+}
