79ef3a50fb6b530b4c47b4a3ff48fbef632d2c69
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-06-24 10:02:47 +0430

    some fields set to notNull()

--- frontend/modules/apartment_rent/migrations/m190620_083827_createTblApartmentRent.php
+++ frontend/modules/apartment_rent/migrations/m190620_083827_createTblApartmentRent.php
@@ -13,21 +13,81 @@ class m190620_083827_createTblApartmentRent extends Migration
     {
         $this->createTable($this->table,[
             'id' => $this->primaryKey(),
-            'area' => $this->integer(),
-            'ad_type_id' => $this->integer(),
-            'ad_advertiser_id' => $this->integer(),
+            'area' => $this->integer()->notNull(),
+            'ad_type_id' => $this->integer()->notNull(),
+            'ad_advertiser_id' => $this->integer()->notNull(),
             'deposit' => $this->integer(),
             'rent_value' => $this->integer(),
-            'room_count_id' => $this->integer(),
-            'created_year' => $this->integer(),
-        ]);
+            'room_count_id' => $this->integer()->notNull(),
+            'created_year_id' => $this->integer(),
+        ],'ENGINE InnoDB');
+
+        $this->addForeignKey(
+            'ad_type_id_apartment_rent_to_ad_type_tbl',
+            $this->table,
+            'ad_type_id',
+            'tbl_ad_type',
+            'id',
+            'NO ACTION',
+            'NO ACTION'
+        );
+
+        $this->addForeignKey(
+            'ad_advertiser_id_apartment_rent_to_ad_advertiser',
+            $this->table,
+            'ad_advertiser_id',
+            'tbl_ad_advertiser',
+            'id',
+            'NO ACTION',
+            'NO ACTION'
+        );
+
+        $this->addForeignKey(
+            'room_count_id_to_room',
+            $this->table,
+            'room_count_id',
+            'tbl_room',
+            'id',
+            'NO ACTION',
+            'NO ACTION'
+        );
+
+        $this->addForeignKey(
+            'created_year_id_to_created_year',
+            $this->table,
+            'created_year_id',
+            'tbl_created_year',
+            'id',
+            'NO ACTION',
+            'NO ACTION'
+        );
+
     }
 
     public function down()
     {
-        echo "m190620_083827_createTblApartmentRent cannot be reverted.\n";
+        $this->dropForeignKey(
+            'created_year_id_to_created_year',
+            $this->table
+        );
 
-        return false;
+        $this->dropForeignKey(
+            'room_count_id_to_room',
+            $this->table
+        );
+
+        $this->dropForeignKey(
+            'ad_advertiser_id_apartment_rent_to_ad_advertiser',
+            $this->table
+        );
+
+        $this->dropForeignKey(
+            'ad_type_id_apartment_rent_to_ad_type_tbl',
+            $this->table
+        );
+
+        $this->dropTable($this->table);
+
     }
 
 }
