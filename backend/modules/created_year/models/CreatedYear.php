25fd50bc177bbd13bfa465297af551e7c6330e2c
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-06-24 10:03:56 +0430

    backend/modules/created_year/models/CreatedYear.php created

---
+++ backend/modules/created_year/models/CreatedYear.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace backend\modules\created_year\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tbl_created_year".
+ *
+ * @property int $id
+ * @property int $title
+ *
+ * @property TblApartmentRent[] $tblApartmentRents
+ */
+class CreatedYear extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'tbl_created_year';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTblApartmentRents()
+    {
+        return $this->hasMany(TblApartmentRent::className(), ['created_year_id' => 'id']);
+    }
+}
