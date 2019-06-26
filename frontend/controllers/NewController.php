d72535918ee9338d67e4d9b85ff75cb16292d890
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-06-26 21:18:40 +0430

    in frontend/controllers/NewController.php -> crate method, we go to create section of every module

--- frontend/controllers/NewController.php
+++ frontend/controllers/NewController.php
@@ -22,7 +22,14 @@ class NewController extends \yii\web\Controller
 
     public function actionCreate($cat_id = null)
     {
-        d(Yii::$app->findModuleComponent->getModuleName($cat_id));
+        $module_name = Yii::$app->findModuleComponent->getModuleName($cat_id);
+        $controller_name = Yii::$app->findModuleComponent->convertModuleToController($module_name);
+        $address = $module_name . '/' . $controller_name;
+
+        Yii::$app->response->redirect([
+            $address . '/create',
+            'cat_id' => $cat_id
+        ]);
     }
 
 }
