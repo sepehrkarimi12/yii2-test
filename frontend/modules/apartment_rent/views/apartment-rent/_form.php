fcc830e12b69ede53bce880fedf5d95b0c565528
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-06-27 14:22:57 +0430

    added ad fields to frontend/modules/apartment_rent/views/apartment-rent/_form.php

--- frontend/modules/apartment_rent/views/apartment-rent/_form.php
+++ frontend/modules/apartment_rent/views/apartment-rent/_form.php
@@ -12,6 +12,10 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
+    <?= $form->field($ad, 'city_id')->textInput() ?>
+
+    <?= $form->field($ad, 'city_range_id')->textInput() ?>
+
     <?= $form->field($model, 'area')->textInput() ?>
 
     <?= $form->field($model, 'ad_type_id')->textInput() ?>
@@ -25,6 +29,23 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'room_count_id')->textInput() ?>
 
     <?= $form->field($model, 'created_year_id')->textInput() ?>
+
+
+
+    <?= $form->field($ad, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($ad, 'desc')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($ad, 'price')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($ad, 'mobile')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($ad, 'immediate')->textInput() ?>
+
+    <?= $form->field($ad, 'chat')->textInput() ?>
+
+    <?= $form->field($ad, 'exchange')->textInput() ?>
+
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
