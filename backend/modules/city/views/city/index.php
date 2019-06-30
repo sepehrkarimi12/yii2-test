de55c8127d7441d32ece292495e432c6c8e5fc5f
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-06-30 14:44:24 +0430

    ui of backend/modules/city/views/city/index.php fixed

--- backend/modules/city/views/city/index.php
+++ backend/modules/city/views/city/index.php
@@ -27,7 +27,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+//            'id',
             'title',
             'sort',
 
