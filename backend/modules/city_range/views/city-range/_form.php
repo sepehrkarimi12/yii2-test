6334e27799cb9b457c6ffd1a01ed49a57ab1f6b1
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-06-30 14:45:53 +0430

    ui of backend/modules/city_range/views/city-range/_form.php fixed

--- backend/modules/city_range/views/city-range/_form.php
+++ backend/modules/city_range/views/city-range/_form.php
@@ -1,6 +1,8 @@
 <?php
 
+use kartik\select2\Select2;
 use yii\helpers\Html;
+use backend\modules\city\models\City;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
@@ -14,7 +16,16 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'city_id')->textInput() ?>
+    <?php
+        echo $form->field($model, 'city_id')->widget(Select2::classname(), [
+            'data' => City::getListForDropDown('id', 'title'),
+            'language' => 'en',
+            'options' => ['placeholder' => 'Select a city...'],
+            'pluginOptions' => [
+                'allowClear' => true
+            ],
+        ]);
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
