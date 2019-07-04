c6b7f3268b865ade0b25d28599ed13e641908481
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-07-04 13:02:03 +0430

    frontend/modules/presell/ created + crud

---
+++ frontend/modules/presell/controllers/PresellController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace frontend\modules\presell\controllers;
+
+use common\models\Ad;
+use frontend\modules\presell\models\Presell;
+use frontend\modules\presell\models\searchModels\PresellSearch;
+use Yii;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * EstateAgensyController implements the CRUD actions for EstateAgensy model.
+ */
+class PresellController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all EstateAgensy models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new PresellSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single EstateAgensy model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new FinanceAndLegal model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate($cat_id = null)
+    {
+        if ($cat_id == null) {
+            return $this->goBack();
+        }
+
+        $model = new Presell();
+        $advertiser = new Ad();
+
+        $advertiser->cat_id = $cat_id;
+
+        if ($model->load(Yii::$app->request->post()) && $advertiser->load(Yii::$app->request->post())
+            && $model->validate() && $advertiser->validate()
+        ) {
+            $model->advertiserModel = $advertiser;
+
+            if ($model->save()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+            'ad' => $advertiser,
+        ]);
+    }
+
+    /**
+     * Updates an existing FinanceAndLegal model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id = null)
+    {
+        if ($id == null) {
+            return $this->goBack();
+        }
+
+        $model = $this->findModel($id);
+        $advertiser = Ad::findOne($model->ad_id);
+
+        if ($model->load(Yii::$app->request->post()) && $advertiser->load(Yii::$app->request->post())
+            && $model->validate() && $advertiser->validate()
+        ) {
+            $model->advertiserModel = $advertiser;
+            if ($model->save()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+            'ad' => $advertiser,
+        ]);
+    }
+
+    /**
+     * Deletes an existing FinanceAndLegal model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the FinanceAndLegal model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return FinanceAndLegal the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Presell::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
