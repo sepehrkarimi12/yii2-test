4fd4e26d49704b0a000445c7ae5e8888f94435b5
sepehrkarimi12 <sepehr.k12@gmail.com>
2019-07-10 00:18:52 +0430

    because of changing tbl_mobile_brand brand to tbl_mobile_and_computer_brand, i have to change relations in migration so i changed in frontend/modules/mobile_and_tablet_mobile/migrations/m190708_214304_craete_tbl_mobile_and_tablet_mobile.php

--- frontend/modules/mobile_and_tablet_mobile/migrations/m190708_214304_craete_tbl_mobile_and_tablet_mobile.php
+++ frontend/modules/mobile_and_tablet_mobile/migrations/m190708_214304_craete_tbl_mobile_and_tablet_mobile.php
@@ -33,7 +33,7 @@ class m190708_214304_craete_tbl_mobile_and_tablet_mobile extends Migration
             'brand_id_mobile_and_tablet_mobile_to_mobile_brand',
             $this->table,
             'brand_id',
-            'tbl_mobile_brand',
+            'tbl_mobile_and_computer_brand',
             'id',
             'NO ACTION',
             'NO ACTION'
